40172abb96982810e94c7704a2c7b3d2bab6e757
khwabscript <59730462+khwabscript@users.noreply.github.com>
2024-07-31 13:38:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ header('Content-type: application/json');
 $uri = $_GET['url'] ?? '';
 $getParams = http_build_query(array_diff_key($_GET, ['url' => true]));
 
-print file_get_contents("https://fantasy.premierleague.com/api/$uri?$getParams");
+try {
+  print file_get_contents("https://fantasy.premierleague.com/api/$uri?$getParams");
+} catch(Exception $e) {
+  print ($_SERVER['SERVER_ADDR'] ?? 'Unknown ip');
+  print $e->getMessage();
+}
 
 return 0;
