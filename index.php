5d106258bb03a67e15ba921b9f919e7cb48ead0c
khwabscript <59730462+khwabscript@users.noreply.github.com>
2026-02-19 17:48:03 +0300

    feat: add base_url params for other APIs

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 
 $uri = $_GET['url'] ?? '';
-$getParams = http_build_query(array_diff_key($_GET, ['url' => true]));
+$baseUrl = $_GET['base_url'] ?? '';
+$getParams = http_build_query(array_diff_key($_GET, ['url' => true, 'base_url' => true]));
 
 try {
     header('Content-type: application/json');
+    if ($baseUrl) {
+        print file_get_contents("$baseUrl/$uri?$getParams");
+        return 0;
+    }
     print file_get_contents("https://fantasy.premierleague.com/api/$uri?$getParams");
 } catch(Exception $e) {
     print ($_SERVER['SERVER_ADDR'] ?? 'Unknown ip') . '<br>';
